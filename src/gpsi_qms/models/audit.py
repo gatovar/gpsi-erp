6bd7ef68a4a17e2755355aea321594970fbf4bb2
Frank Mendoza <francisco.mendoza@globalstd.com>
2016-11-22 10:54:49 -0600

    Elimina proyectos innecesarios

--- src/gpsi_qms/models/audit.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
-
-
-class AuditOrder(models.Model):
-    _name = 'gpsi.qms.audit.order'
-    _description = 'Audit Order'
