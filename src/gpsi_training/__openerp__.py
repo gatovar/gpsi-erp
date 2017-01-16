ceb90ee6888d662d562a2d71490cb4f3ea6e11a9
Frank Mendoza <desarrollo@globalstd.com>
2017-01-16 13:40:47 -0600

    Agrega modulo "training"

--- src/gpsi_training/__openerp__.py
+++ src/gpsi_training/__openerp__.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'GPSI CRM',
+    'name': 'GPSI Training',
     'version': '1.0.0',
     'category': 'globalstd',
     'sequence': 20,
-    'summary': 'CRM Extension',
+    'summary': 'GPSI Training',
     'description': '',
     'website': 'http://www.globalstd.com/',
-    'depends': ['mail', 'crm'],
-    'data': [
-        'security/crm_security.xml',
-        'views/crm_views.xml',
-        'data/crm_data.xml',
-        'data/crm_auto_actions.xml'
-    ],
+    'depends': [],
+    'data': [],
     'test': [],
     'installable': True,
     'auto_install': False,
