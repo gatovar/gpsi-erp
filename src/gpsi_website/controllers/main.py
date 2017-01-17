5671c0f793ecf9bff74412f8f3872e2ce9bcd28b
Frank Mendoza <desarrollo@globalstd.com>
2017-01-17 11:54:52 -0600

    Agrega página signin

--- src/gpsi_website/controllers/main.py
+++ src/gpsi_website/controllers/main.py
@@ -9,6 +9,14 @@ from openerp.http import request
 
 
 class Website(http.Controller):
+    @http.route('/gs/signin', type='http', auth="none")
+    def signin(self, **kw):
+        return request.render('gpsi_website.signin')
+
+    @http.route('/gs/signup', type='http', auth="none")
+    def signup(self, **kw):
+        return request.render('gpsi_website.signup')
+
     @http.route('/gs', type='http', auth="none")
     def home(self, **kw):
         return request.render('gpsi_website.home')
