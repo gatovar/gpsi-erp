0262d303af55707c3bec83b4df337c0c3bd6d780
Eduardo Vasquez <evazquez@globalstd.com>
2017-01-17 18:27:26 -0600

    Agregar botón a documento de empleado #9

--- src/gpsi_staff/models/skill.py
+++ src/gpsi_staff/models/skill.py
@@ -16,6 +16,33 @@ class Employee(models.Model):
     _inherit = 'hr.employee'
 
     gs_competence_ids = fields.One2many('gpsi.staff.competence', 'employee_id', 'Competences')
+    doc_count = fields.Integer(compute='_get_doc_count', index=True)
+
+    @api.multi
+    def _get_doc_count(self):
+        attachments = self.env['ir.attachment'].search([('res_model', '=', 'hr.employee'), ('res_id', '=', self.id)])
+        self.doc_count = len(attachments)
+
+    @api.multi
+    def attachment_tree_view(self):
+        domain = [('res_model', '=', 'hr.employee'), ('res_id', '=', self.id)]
+
+        return {
+            'name': _('Attachments'),
+            'domain': domain,
+            'res_model': 'ir.attachment',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'kanban,tree,form',
+            'view_type': 'form',
+            'help': _('''<p class="oe_view_nocontent_create">
+                        Documents are attached to the tasks and issues of your project.</p><p>
+                        Send messages or log internal notes with attachments to link
+                        documents to your project.
+                    </p>'''),
+            'limit': 80,
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id)
+        }
 
 class Competence(models.Model):
     _name = 'gpsi.staff.competence'
