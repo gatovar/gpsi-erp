4194c07eaba9f736282ec5bc5cb6ed06c5503ea4
Eduardo Vasquez <evazquez@globalstd.com>
2017-01-20 14:02:45 -0600

    Cambios #22

--- src/gpsi_core/models/hr_employee_profile.py
+++ src/gpsi_core/models/hr_employee_profile.py
@@ -11,82 +11,49 @@ class EmployeeProfile(models.Model):
 
     _name = 'hr.employee.profile'
     _description = 'Employee Profile'
-
+    
     employee_id = fields.Many2one('hr.employee', 'Employee')
-    customer_id = fields.Many2one('res.partner', 'Customer')
-    experience_ids = fields.One2many('hr.employee.profile.employment.experience', 'experience_id', 'Employment Experience')
-    training_ids = fields.One2many('hr.employee.profile.job.training', 'training_id', 'Job Training')
-    historial_ids = fields.One2many('hr.employee.profile.academic.historial', 'college_id', 'Academic Historial')
-    courses_ids = fields.One2many('hr.employee.profile.completed.courses', 'course_id', 'Completed Courses')
+    summary = fields.Char('Certifications Summary')
+    customer_ids = fields.One2many('hr.employee.profile.customers', 'client_id', 'Customer')
+    experience_ids = fields.One2many('hr.employee.profile.experience', 'experience_id', 'Experience')
+    education_ids = fields.One2many('hr.employee.profile.education', 'education_id', 'Education')
 
-class EmploymentExperience(models.Model):
+class EmployeeCustomers(models.Model):
+    '''
+    Model para listar empresas auditadas por el empleado.
+    '''
+
+    _name = 'hr.employee.profile.customers'
+    _description = 'Employee Customers'
+
+    company_id = fields.Many2one('res.partner', 'Profile')
+    client_id = fields.Many2one('hr.employee.profile', 'Profile')
+
+class EmployeeExperience(models.Model):
     '''
     Model para listar experiencia laboral del empleado.
     '''
 
-    _name = 'hr.employee.profile.employment.experience'
-    _descripcion = 'Employment Experience'
+    _name = 'hr.employee.profile.experience'
+    _descripcion = 'Employee Experience'
     
-    job_position = fields.Char('Job Position')
+    company_id = fields.Many2one('res.partner', 'Company')
+    job_position = fields.Char('Position')
+    job_description = fields.Char('Description')
     start_date = fields.Date('Start Date')
     end_date = fields.Date('End Date')
     experience_id = fields.Many2one('hr.employee.profile', 'Profile')
 
-class JobTraining(models.Model):
-    '''
-    Model para listar formación y conocimientos del empleado.
-    '''
-
-    _name = 'hr.employee.profile.job.training'
-    _description = 'Job Training'
-
-    expertise_area = fields.Char('Area of Expertise')
-    start_date = fields.Date('Start Date')
-    end_date = fields.Date('End Date')
-    training_id = fields.Many2one('hr.employee.profile', 'Profile', ondele="cascade")
-
-class AcademicHistorial(models.Model):
+class EmployeeEducation(models.Model):
     '''
     Model para lista el historial académico del empleado.
     '''
 
-    _name = 'hr.employee.profile.academic.historial'
-    _description = 'Academic Historial'
+    _name = 'hr.employee.profile.education'
+    _description = 'Employee Education'
 
-    study_grade = fields.Char('Study Grade')
-    institute = fields.Char('Alma Mater')
+    study_level = fields.Char('Study Grade')
+    alma_mater = fields.Char('Alma Mater')
     start_date = fields.Date('End Date')
     end_date = fields.Date('End Date')
-    college_id = fields.Many2one('hr.employee.profile', 'Profile', ondele="cascade")
-
-class CompletedCourses(models.Model):
-    '''
-    Model para lista los cursos fianalizados por el empleado.
-    '''
-
-    _name = 'hr.employee.profile.completed.courses'
-    _description = 'Completed Courses'
-
-    course = fields.Char('Course')
-    start_date = fields.Date('End Date')
-    end_date = fields.Date('End Date')
-    course_id = fields.Many2one('hr.employee.profile', 'Profile', ondele="cascade")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    education_id = fields.Many2one('hr.employee.profile', 'Profile', ondele="cascade")
